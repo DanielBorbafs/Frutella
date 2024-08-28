9e51ec8c2ff49e141a914ae518e38461d074d187
DanielBorbafs <dev.danielborba@gmail.com>
2024-08-27 22:13:22 -0300

    fix: section hero

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -1,18 +1,14 @@
-import './Hero.css'
-import lemonImage from '../../assets/images/lemon.png';
+import styles from './Hero.module.css'
+import HeroContent from '../../Components/HeroContent'
 
-const Hero = () => {
-    return(
-        <section className="container hero-section">
-            <div className="hero-section_text col-5">
-                <span>Frutella</span>
-                <h1>Sucos naturais e refrescantes para todos os momentos!</h1>
-                <p>descubra o sabor autêntico das frutas em cada gole. Na Frutella, nossos sucos são feitos com ingredientes frescos e selecionados, trazendo para você o melhor da natureza em bebidas saudáveis e deliciosas. Experimente nossos sabores únicos e refresque seu dia!</p>
-            </div>
-            <div className="col-6"> 
-                <img src={lemonImage} alt="imagem de um copo de suco de limão"/>
-            </div>
+
+function Hero() {
+    return (
+        <section className={styles.heroSection}>
+            <HeroContent />
         </section>
+
+
 
     )
 }
