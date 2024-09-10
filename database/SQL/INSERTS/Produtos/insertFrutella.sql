e088a42156e75a23679f175bfe2ddaee0916e952
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2024-09-10 00:31:25 -0300

    Create insertFrutella.sql

---
+++ database/SQL/INSERTS/Produtos/insertFrutella.sql
@@ -0,0 +1,11 @@
+INSERT INTO PRODUTO VALUES
+('FRT00001', 'Frutella Frescor de verão ', 'Lata', '350ml', 'Limão', 2.80, 'Frutella'),
+('FRT00002', 'Frutella Frescor de inverno ', 'Lata', '400ml', 'Menta', 3.50, 'Frutella'),
+('FRT00003', 'Frutella Frescor de verão ', 'Litro', '600ml', 'Laranja', 7.70, 'Frutella')
+GO
+
+INSERT INTO ESTOQUE VALUES
+(19, 'FRT00001', 100, 'Frutella', 2.80),
+(20, 'FRT00002', 80, 'Frutella', 2.80),
+(21, 'FRT00003', 100, 'Frutella', 5.50)
+GO
